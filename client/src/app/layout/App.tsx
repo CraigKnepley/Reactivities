f7157c5bb4ce12c19aedd4ccb53b2960f8ce3b57
Craig Knepley <cbknepley@gmail.com>
2025-03-05 15:57:21 -0800

    Added View/Cancel button functionality

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,4 +1,4 @@
-import { Container, CssBaseline } from "@mui/material";
+import { Box, Container, CssBaseline } from "@mui/material";
 import { useEffect, useState } from "react"
 import axios from "axios";
 import NavBar from "./NavBar";
@@ -7,6 +7,7 @@ import ActivityDashboard from "../../features/activities/dashboard/ActivityDashb
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
 
   useEffect(() => {
     axios.get<Activity[]>('http://localhost:5001/api/activities')
@@ -15,14 +16,27 @@ function App() {
     return () => {}
   }, [])
 
+  const handleSelectActivity = (id: string) => {
+    setSelectedActivity(activities.find(x => x.id === id));
+  }  
+
+  const handleCancelSelectActivity = () => {
+    setSelectedActivity(undefined);
+  }
+
   return (
-    <>
+    <Box sx={{bgcolor: '#eeeeee'}}>
       <CssBaseline />
       <NavBar />
       <Container maxWidth='xl' sx={{mt: 3}}>
-        <ActivityDashboard activities={activities} />
+        <ActivityDashboard
+          activities={activities}
+          selectActivity={handleSelectActivity}
+          cancelSelectActivity={handleCancelSelectActivity}
+          selectedActivity={selectedActivity}
+        />
       </Container>
-    </>
+    </Box>
     
   )
 }
